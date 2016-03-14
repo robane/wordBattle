b47735e18c21cd5bd7dbcee3bb8d03a8623a1711
robane <robert.aneszko@gmail.com>
2016-03-14 11:03:42 +0000

    second commit

--- source/app.js
+++ source/app.js
@@ -5,6 +5,10 @@ app.get("/", function(req, res) {
     res.send("Hello, world!");
 });
 
+app.get("/createUser", function(req, res) {
+    res.json({ success: false });
+});
+
 app.listen(8080, function() {
     console.log("listening...");
 });
